01971f3eceb659911de001850e0287c6d892c740
LUANA HYPARXIS <luhyxi@gmail.com>
2023-10-01 14:32:38 -0300

    New Stuff

---
+++ C# Number 2 Now/AdventCode12022/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using CsvHelper;
+using System.IO;
+using System.Globalization;
+using System.Linq;
+
+namespace AdventCode_1_2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 3.14;
+            int b = Convert.ToInt32(a)
+            using (var streamReader =
+                   new StreamReader(@"/home/luhyxi/Codeee/Rabiskos/C# Number 2 Now/AdventCode12022/adventofcode.csv"))
+            {
+                using (var csvReader = new CsvReader(streamReader,CultureInfo.InvariantCulture))
+                {
+                    var records = csvReader.GetRecords<dynamic>().ToList();
+                }
+            }
+        }
+    }
+}
