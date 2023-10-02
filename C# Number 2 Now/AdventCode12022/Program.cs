590a3a7f82b034b3ab21d5773565ad0d2dfac7a0
LUANA HYPARXIS <luhyxi@gmail.com>
2023-10-02 13:56:00 -0300

    More C# study

--- C# Number 2 Now/AdventCode12022/Program.cs
+++ C# Number 2 Now/AdventCode12022/Program.cs
@@ -1,25 +1,68 @@
 ﻿using System;
 using CsvHelper;
+using CsvHelper.Configuration;
 using System.IO;
 using System.Globalization;
 using System.Linq;
+using System.Collections.Generic; // Add this for List
 
 namespace AdventCode_1_2022
 {
+    public class CsvRecord
+    {
+        public int Calories { get; set; }
+        public bool IsEmpty { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            double a = 3.14;
-            int b = Convert.ToInt32(a)
-            using (var streamReader =
-                   new StreamReader(@"/home/luhyxi/Codeee/Rabiskos/C# Number 2 Now/AdventCode12022/adventofcode.csv"))
+            List<int> sums = new List<int>(); // Moved the 'sums' list declaration outside the CsvReader scope.
+
+            using (var streamReader = new StreamReader(@"/home/luhyxi/Codeee/Rabiskos/C# Number 2 Now/AdventCode12022/adventofcode.csv"))
+            using (var csvReader = new CsvReader(streamReader, new CsvConfiguration(CultureInfo.InvariantCulture)
             {
-                using (var csvReader = new CsvReader(streamReader,CultureInfo.InvariantCulture))
+                HasHeaderRecord = false,// Indicates that the CSV file doesn't have a header row.
+                MissingFieldFound = null // Ignore missing fields
+            }))
+            {
+                var records = csvReader.GetRecords<CsvRecord>().ToList();
+
+                foreach (var record in records)
                 {
-                    var records = csvReader.GetRecords<dynamic>().ToList();
+                    if (record.Calories == null) // You can adjust this condition as needed.
+                    {
+                        record.IsEmpty = true;
+                    }
+                }
+
+                int currentSum = 0;
+
+                foreach (var record in records)
+                {
+                    if (record.IsEmpty)
+                    {
+                        if (currentSum > 0)
+                        {
+                            sums.Add(currentSum);
+                        }
+                        currentSum = 0;
+                    }
+                    else
+                    {
+                        currentSum += record.Calories;
+                    }
+                }
+
+                if (currentSum > 0)
+                {
+                    sums.Add(currentSum);
                 }
             }
+
+            // At this point, 'sums' contains the sums of data between rows with no data.
+            // You can use 'sums' as needed.
         }
     }
 }
